4e72153d906fb5c44d93fd9d00575fad4051700a
danielk43 <11856117+danielk43@users.noreply.github.com>
2022-02-18 20:32:50 -0500

    Update autoupdater apps

--- LocalGsmNlpBackend/Android.mk
+++ LocalGsmNlpBackend/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := LocalGsmNlpBackend
-LOCAL_SRC_FILES := org.fitchfamily.android.gsmlocation_73.apk
+LOCAL_SRC_FILES := LocalGsmNlpBackend.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
